6141f4c7687631e24dce596511cde4918412a1cd
chopa <chopa@10.1.10.18>
2019-04-29 16:19:26 -0500

    Checked!

--- src/Constructors/DebitCard.java
+++ src/Constructors/DebitCard.java
@@ -1,34 +1,38 @@
 package Constructors;
 
 public class DebitCard {
-	long cardNumber;
+	String cardNumber;
 	String holderName;
 	String type;
 	int pin;
 	double balance;
-	public DebitCard(long cardNumber, String holderName, int pin, double balance, String type) {
+	public DebitCard(String cardNumber, String holderName, int pin, double balance, String type) {
 		this.cardNumber = cardNumber;
 		this.holderName = holderName;
 		this.pin = pin;
 		this.balance = balance;
 		
-		if(!type.equals("Mastercard") || !type.equals("Visa")) {
-			System.out.println("Invalid type!");
-		}else {
+		if(type.equalsIgnoreCase("Mastercard") || type.equalsIgnoreCase("Visa")) {
 			this.type = type;
+			
+		}else {
+			System.out.println("Invalid type!");
+			
 		}
 		String pinL = pin + "";
-		if(pinL.length()= 4) {
-			System.out.println("Invalid pin");
-		}else {
+		if(pinL.length()== 4) {
 			this.pin = pin;
+			
+		}else {
+			System.out.println("Invalid pin");
+			
 		}
 		
 		String cardL = cardNumber + "";
-		if(cardL.length() != 16) {
-			System.out.println("Invalid card number");
+		if(cardL.length() == 16) {
+			this.cardNumber = cardNumber;	
 		}else {
-			this.cardNumber = cardNumber;
+			System.out.println("Invalid card number");	
 		}
 		
 		
--- src/Constructors/DebitCardTester.java
+++ src/Constructors/DebitCardTester.java
@@ -2,7 +2,7 @@ package Constructors;
 
 public class DebitCardTester {
 	public static void main(String[] args) {
-		DebitCard db1 = new DebitCard(123456789L, "Cholpon Shabolotova", 4478, 4000.00, "Visa");
+		DebitCard db1 = new DebitCard("1234567890123456", "Cholpon Shabolotova", 4588, 4000.0, "Mastercard");
 	}
 
 }
